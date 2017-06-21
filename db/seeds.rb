911604ae72319dbb73a1d03436e2c07edbba87c9
Kathryn Tipton <kathryn.tipton@gmail.com>
2017-06-20 20:22:53 -0400

    Beginnings of test seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+mealplans = MealPlan.create(
+  [ { name: "Test Plan 1: The Salads", week: Date.today, filling: 3, longevity: 4, rating: 5, prep_time: 180, notes: "That's a meal plan" } ] )
+
+recipes = Recipe.create ( [
+  { name: "Salad A", notes: "healthy", calories: "300", protein: "20", lipids: "15", carbs: "40", nutrition_url: "cronometer.com", recipe_url: "ohsheglows.com"},
+  { name: "Salad B", notes: "less healthy", calories: "900", protein: "20", lipids: "50", carbs: "120", nutrition_url: "cronometer.com", recipe_url: "ohsheglows.com" } ] )
+
+
+# TK
+# food1 = Food.create( rating: 5, david_rating: 1, longevity: 5, notes: "Divisive", meal_plan: mealplans.first, recipe: recipes.first )
