a974ce10baeb38babf48ce839f13e47cfe4ed159
Kathryn Tipton <kathryn.tipton@gmail.com>
2017-06-21 21:25:36 -0400

    Working tests!

---
+++ test/controllers/foods_controller_test.rb
@@ -0,0 +1,60 @@
+require 'test_helper'
+
+class FoodsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @food = foods(:one)
+  end
+
+  test "should get index" do
+    get foods_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get new_food_url
+    assert_response :success
+  end
+
+  test "should create food" do
+    assert_difference('Food.count') do
+      post foods_url, params: {
+        food: {
+          rating: @food.rating,
+          david_rating: @food.david_rating,
+          longevity: @food.longevity,
+          notes: @food.notes,
+       }}
+    end
+
+    assert_redirected_to food_url(Food.last)
+  end
+
+  test "should show food" do
+    get food_url(@food)
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get edit_food_url(@food)
+    assert_response :success
+  end
+
+  test "should update food" do
+    patch food_url(@food), params: {
+      food: {
+        rating: @food.rating,
+        david_rating: @food.david_rating,
+        longevity: @food.longevity,
+        notes: @food.notes,
+    }}
+    assert_redirected_to food_url(@food)
+  end
+
+  test "should destroy food" do
+    assert_difference('Food.count', -1) do
+      delete food_url(@food)
+    end
+
+    assert_redirected_to foods_url
+  end
+end
